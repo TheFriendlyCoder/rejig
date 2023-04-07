1b8761fba559ec5e44683767908cb45cc285fa80
Kevin Phillips <kevinp@ca.ibm.com>
2023-04-07 13:45:40 -0300

    Reworked tests to use shell wrapper

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
-#!/usr/bin/env sh -e
+#!/usr/bin/env bash -e
 # Compile a release for the current platform
 #goreleaser build --single-target --clean --snapshot
+
+# Run linter
 golangci-lint run
-# Run all tests recursively
-#go test ./...
 
-# With coverage
-#go test --cover ./...
-
-# With HTML report
-go test -coverprofile cover.out ./...
-go tool cover -html=cover.out
+# Run tests
+./test.sh
---
+++ test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash -e
+
+# minimum code coverage we expect from our code project
+TESTCOVERAGE_THRESHOLD=79
+# Red ASCII color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+# NoColor reset code
+NC='\033[0m'
+
+# Run all tests recursively
+#go test ./...
+
+# With shell coverage report
+#go test --cover ./...
+
+# Run tests and store coverage report to disk
+go test ./... -coverprofile coverage.out -covermode set
+
+# Remove ignored files from coverage report
+declare -a ignoreFiles=("lib/aferoWrapper.go")
+for curFile in "${ignoreFiles[@]}"; do
+  sed -i '' "/${curFile//\//\\/}/d" ./coverage.out
+done
+
+# Display ASCII report to console
+go tool cover -func coverage.out
+# Display HTML report in default browser
+#go tool cover -html=coverage.out
+
+# Check coverage threshold
+# Extract the coverage total from the coverage report. The line being parsed should look
+# something like this:
+#       total:								(statements)		12.3%
+# and we extract the "12.3" decimal number from the line
+totalCoverage=$(go tool cover -func=coverage.out | grep "^total" | grep -Eo '[0-9]+\.[0-9]+')
+# Here we check 2 floating point values for percentage coverage using awk
+if (( $(echo "$totalCoverage $TESTCOVERAGE_THRESHOLD" | awk '{print ($1 > $2)}') )); then
+    echo -e "${GREEN}Coverage of ${totalCoverage}% meets minimum ${TESTCOVERAGE_THRESHOLD}%.${NC}"
+else
+    echo -e"${RED}Coverage of ${totalCoverage}% doesn't meet minimum coverage of ${TESTCOVERAGE_THRESHOLD}%.${NC}"
+    exit 1
+fi
