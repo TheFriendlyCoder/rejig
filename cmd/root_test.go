01c6b156d74ecfb5cb0bbf3466eceaa15c9e6391
Kevin Phillips <kevinp@ca.ibm.com>
2023-04-16 09:21:56 -0300

    Removed configurable config file path flag

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -4,54 +4,84 @@ import (
 	"bytes"
 	"os"
 	"path"
+	"runtime"
 	"testing"
 
-	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+// setHome helper function that overloads the default user home folder
+// to point to a custom location for our test
+func setHome(t *testing.T, newHome string) string {
+	envVar := "HOME"
+	switch runtime.GOOS {
+	case "windows":
+		envVar = "USERPROFILE"
+	case "plan9":
+		envVar = "home"
+	}
+	oldHome, err := os.UserHomeDir()
+	require.NoError(t, err)
+	require.NoError(t, os.Setenv(envVar, newHome))
+	return oldHome
+}
+
+func restoreHome(t *testing.T, newHome string) {
+	envVar := "HOME"
+	switch runtime.GOOS {
+	case "windows":
+		envVar = "USERPROFILE"
+	case "plan9":
+		envVar = "home"
+	}
+	require.NoError(t, os.Setenv(envVar, newHome))
+}
+
 func Test_helpCommand(t *testing.T) {
 	r := require.New(t)
+	a := assert.New(t)
 
+	// Redirect our user home folder to an empty temp dir
+	// to force the command to load default options
 	tmpDir, err := os.MkdirTemp("", "")
 	r.NoError(err)
 	defer os.RemoveAll(tmpDir)
 
-	// Point our command to an empty config file so the test suite
-	// doesn't try and read config options from the users home folder
-	configFile := path.Join(tmpDir, "config.yml")
-	fh, err := os.Create(configFile)
-	r.NoError(err)
-	_, err = fh.WriteString("")
-	r.NoError(err)
-	r.NoError(fh.Close())
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
 
-	// When we run the root command with a custom config file
+	// When we run the help command
 	actual := new(bytes.Buffer)
 	rootCmd.SetOut(actual)
 	rootCmd.SetErr(actual)
 
-	rootCmd.SetArgs([]string{"--config", configFile, "help"})
+	rootCmd.SetArgs([]string{"help"})
 	err = rootCmd.Execute()
 
-	// We expect no error from our command
-	r.NoError(err, "CLI command should have succeeded")
-	r.Contains(actual.String(), "rejigger")
+	r.NoError(err)
+	a.Contains(actual.String(), "rejigger")
 }
 
 func Test_executeConfigFileInvalidYAML(t *testing.T) {
 	r := require.New(t)
 
+	// Redirect our user home folder to a temp dir
+	// to force the command to load default options
 	tmpDir, err := os.MkdirTemp("", "")
 	r.NoError(err)
 	defer os.RemoveAll(tmpDir)
 
-	configFile := path.Join(tmpDir, "sample.yml")
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
+
+	// create an app config with invalid YAML
+	configFile := path.Join(tmpDir, ".rejig")
 	fh, err := os.Create(configFile)
 	r.NoError(err)
-	_, err = fh.WriteString("templates: fubar")
+	// _, err = fh.WriteString("templates: fubar")
+	_, err = fh.WriteString("not valid yaml")
 	r.NoError(err)
 	r.NoError(fh.Close())
 
@@ -60,43 +90,90 @@ func Test_executeConfigFileInvalidYAML(t *testing.T) {
 	rootCmd.SetOut(actual)
 	rootCmd.SetErr(actual)
 
-	rootCmd.SetArgs([]string{"--config", configFile, "help"})
+	rootCmd.SetArgs([]string{"help"})
 	err = rootCmd.Execute()
 	r.Error(err)
 }
 
 func Test_initConfigDefaults(t *testing.T) {
 	r := require.New(t)
+	a := assert.New(t)
 
+	// Redirect our user home folder to an empty temp dir
+	// to force the command to load default options
+	tmpDir, err := os.MkdirTemp("", "")
+	r.NoError(err)
+	defer os.RemoveAll(tmpDir)
+
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
+
+	// When we try to init a new config
 	v := viper.New()
+	opts, err := initConfig(v)
 
-	_, err := initConfig(v, "")
-	_, err2 := os.Stat(v.ConfigFileUsed())
-	// On CI the default config file won't be found
-	if os.IsNotExist(err2) {
-		// and in this case an error is triggered
-		r.Error(err)
-	} else {
-		r.NoError(err)
-	}
+	// We expect the operation to succeed
+	r.NoError(err)
 
-	// NOTE: we can't validate the results of the parse operation here
-	// because the results of the default parsing will depend on the
-	// users config file in the current home folder
+	// And a set of default options to be returned
+	a.Equal(0, len(opts.Templates))
+	a.Equal(0, len(opts.Inventories))
+}
+
+func Test_initConfigSuccessfulParse(t *testing.T) {
+	r := require.New(t)
+	a := assert.New(t)
+
+	// Redirect our user home folder to an empty temp dir
+	// to force the command to load default options
+	tmpDir, err := os.MkdirTemp("", "")
+	r.NoError(err)
+	defer os.RemoveAll(tmpDir)
+
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
+
+	// Create a valid config file
+	configFile := path.Join(tmpDir, ".rejig")
+	fh, err := os.Create(configFile)
+	r.NoError(err)
+	_, err = fh.WriteString("")
+	r.NoError(err)
+	r.NoError(fh.Close())
+
+	// When we try to init a new config
+	v := viper.New()
+	opts, err := initConfig(v)
+
+	// We expect the operation to succeed
+	r.NoError(err)
+
+	// And a set of default options to be returned
+	a.Equal(0, len(opts.Templates))
+	a.Equal(0, len(opts.Inventories))
 }
 
 func Test_initConfigFileNotExist(t *testing.T) {
 	r := require.New(t)
 	a := assert.New(t)
 
-	v := viper.New()
-
+	// Redirect our user home folder, which is where the application
+	// will look for an app config file
 	tmpDir, err := os.MkdirTemp("", "")
 	r.NoError(err)
 	defer os.RemoveAll(tmpDir)
 
-	opts, err := initConfig(v, path.Join(tmpDir, "fubar.yml"))
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
+
+	// When we attempt to initialize our app config
+	v := viper.New()
+	opts, err := initConfig(v)
+
+	// We should not get any errors
 	r.NoError(err)
+
+	// And default options should be returned
 	a.Equal(0, len(opts.Templates))
 	a.Equal(0, len(opts.Inventories))
 }
@@ -104,42 +181,78 @@ func Test_initConfigFileNotExist(t *testing.T) {
 func Test_initConfigFileNoPermission(t *testing.T) {
 	r := require.New(t)
 
-	v := viper.New()
-
+	// Redirect our user home folder, which is where the application
+	// will look for an app config file
 	tmpDir, err := os.MkdirTemp("", "")
 	r.NoError(err)
 	defer os.RemoveAll(tmpDir)
 
-	outputDir := path.Join(tmpDir, "test")
-	r.NoError(os.Mkdir(outputDir, 0600))
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
 
-	_, err = initConfig(v, path.Join(outputDir, "fubar.yml"))
+	// Create a valid config file but make it read-only so the app
+	// can read the contents
+	configFile := path.Join(tmpDir, ".rejig")
+	fh, err := os.Create(configFile)
+	r.NoError(err)
+	_, err = fh.WriteString("")
+	r.NoError(err)
+	r.NoError(fh.Chmod(0200))
+	r.NoError(fh.Close())
+
+	// When we attempt to initialize our app
+	v := viper.New()
+	_, err = initConfig(v)
+
+	// The operation should fail
 	r.Error(err)
 }
 
 func Test_initConfigFileInvalidYAML(t *testing.T) {
 	r := require.New(t)
 
-	v := viper.New()
-
+	// Redirect our user home folder, which is where the application
+	// will look for an app config file
 	tmpDir, err := os.MkdirTemp("", "")
 	r.NoError(err)
 	defer os.RemoveAll(tmpDir)
 
-	configFile := path.Join(tmpDir, "sample.yml")
+	oldHome := setHome(t, tmpDir)
+	defer restoreHome(t, oldHome)
+
+	// Create an app options file
+	configFile := path.Join(tmpDir, ".rejig")
 	fh, err := os.Create(configFile)
 	r.NoError(err)
 	_, err = fh.WriteString("templates: fubar")
 	r.NoError(err)
 	r.NoError(fh.Close())
 
-	_, err = initConfig(v, configFile)
+	// Attempt to load the app config
+	v := viper.New()
+	_, err = initConfig(v)
+
+	// The operation should fail
 	r.Error(err)
 }
 
-func Test_checkErr(t *testing.T) {
+func Test_initConfigMissingHome(t *testing.T) {
 	r := require.New(t)
 
-	r.Panics(func() { checkErr(errors.New("Error")) })
-	r.NotPanics(func() { checkErr(nil) })
+	// Redirect our user home folder to an empty temp dir
+	// to force the command to load default options
+	tmpDir, err := os.MkdirTemp("", "")
+	r.NoError(err)
+	defer os.RemoveAll(tmpDir)
+
+	oldHome := setHome(t, "")
+	defer restoreHome(t, oldHome)
+
+	// When we try to init a new config
+	// We expect the operation to panic
+	v := viper.New()
+	r.Panics(func() {
+		_, err = initConfig(v)
+		r.NoError(err)
+	})
 }
